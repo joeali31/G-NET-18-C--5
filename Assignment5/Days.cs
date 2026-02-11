f204b6fd800dba1ed016960431709b04c29c95a0
joeali31 <ya9415565@gmail.com>
2026-02-11 10:18:04 -0800

    Question1

---
+++ Assignment5/Days.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    enum Days
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+}
--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            
+
+            #region Question1
+            //int dayNumber;
+            //bool isParsed;
+
+            //do
+            //{
+            //    Console.Write("Enter a day number (1-7): ");
+
+            //    isParsed = int.TryParse(Console.ReadLine(), out dayNumber);
+            //    dayNumber -= 1;
+
+            //    if (!Enum.IsDefined(typeof(Days), dayNumber))
+            //    {
+            //        isParsed = false;
+            //    }
+            //}
+            //while (!isParsed);
+
+            //Days day = (Days)dayNumber;
+
+            //switch (day)
+            //{
+            //    case Days.Saturday:
+            //    case Days.Friday:
+            //        Console.WriteLine($"\tDay: {day}");
+            //        Console.WriteLine($"\tit's the Weekend \n");
+            //        break;
+            //    default:
+            //        Console.WriteLine($"\tDay: {day}");
+            //        Console.WriteLine($"\tit's the Workday \n");
+            //        break;
+            //}
+            #endregion
+
         }
     }
 }
